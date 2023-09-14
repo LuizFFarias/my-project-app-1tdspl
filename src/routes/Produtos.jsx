f3cde489d6d1252aaf364bcfd574a13b7e3880c8
LuizFFarias <rm99519@fiap.com.br>
2023-09-14 11:37:23 -0300

    commit

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,4 +1,7 @@
 import { ListaProdutos } from "../compoentes/ListaProdutos";
+import { Link } from "react-router-dom";
+import classes from "./Produtos.module.css"
+import {AiFillEdit as Editar} from "react-icons/ai"
 
 export default function Produtos(){
     return(
@@ -6,7 +9,7 @@ export default function Produtos(){
             <h1>Componentes Produtos</h1>
             <p>Onde os produtos nascem</p>
             
-            <table style={{borderCollapse: "collapse", border: "2px solid #ffffff"}}>
+            <table className={classes.estilo}>
                 <thead>
                     <tr>
                         <th style={{border: "2px solid #fffff"}}>ID</th>
@@ -17,16 +20,19 @@ export default function Produtos(){
                 </thead>
                 <tbody>
                     {ListaProdutos.map((produto, indice)=>(
-                        <tr key={indice}>
+                        <tr key={indice} className={classes.tableLine}>
                             <td>{produto.id}</td>
                             <td>{produto.nome}</td>
                             <td>{produto.desc}</td>
                             <td>{produto.preco}</td>
+                            <td>
+                                <Link to={`/editar/produtos/${produto.id}`}></Link>
+                            </td>
 
                         </tr>                        
                     ))}
                 </tbody>
-                <tfoot>
+                <tfoot className={classes.tableFooter}>
                     <tr>
                         <td colSpan={3}>Produtos</td>
                     </tr>
