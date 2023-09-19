549e04bff37945161f14d6a8e35b118bbdf4be69
LuizFFarias <rm99519@fiap.com.br>
2023-09-19 09:53:20 -0300

    Counter adicionad o

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 
-import Secao from './compoentes/Secao'
-import Rodape from './compoentes/Rodape'
-import viteLogo from './assets/vite.svg'
-import Lista from './compoentes/Lista'
+import Rodape from './components/Rodape'
+import Lista from './components/Lista'
 import { Outlet } from 'react-router-dom';
-
 
 export default function App(){
 
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,43 +1,72 @@
-import { ListaProdutos } from "../compoentes/ListaProdutos";
 import { Link } from "react-router-dom";
-import classes from "./Produtos.module.css"
-import {AiFillEdit as Editar} from "react-icons/ai"
+import { ListaProdutos } from "../components/ListaProdutos";
+import classes from "./Produtos.module.css";
+import {AiFillEdit as Editar} from "react-icons/ai";
+import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
+import { useEffect, useState } from "react";
 
-export default function Produtos(){
-    return(
-        <div>
-            <h1>Componentes Produtos</h1>
-            <p>Onde os produtos nascem</p>
-            
-            <table className={classes.estilo}>
-                <thead>
-                    <tr>
-                        <th style={{border: "2px solid #fffff"}}>ID</th>
-                        <th style={{border: "2px solid #fffff"}}>Nome</th>
-                        <th style={{border: "2px solid #fffff"}}>Descricao</th>
-                        <th style={{border: "2px solid #fffff"}}>Preco</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {ListaProdutos.map((produto, indice)=>(
-                        <tr key={indice} className={classes.tableLine}>
-                            <td>{produto.id}</td>
-                            <td>{produto.nome}</td>
-                            <td>{produto.desc}</td>
-                            <td>{produto.preco}</td>
-                            <td>
-                                <Link to={`/editar/produtos/${produto.id}`}></Link>
-                            </td>
+export default function Produtos() {
 
-                        </tr>                        
-                    ))}
-                </tbody>
-                <tfoot className={classes.tableFooter}>
-                    <tr>
-                        <td colSpan={3}>Produtos</td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-    )
+
+  const [counter, setCounter] = useState(0);
+  document.title = "Lista de Produtos: " + counter;
+  useEffect(() => (
+     console.log("Este useEffects renderiza sempre que ocorre alguma atualização no componente!")
+  ));
+const [listaProdutosLocal, setListaProduosLocal] = useState([{}]);
+
+  useEffect(() => {
+    console.log("Esse useEffect renderiza apenas uma vez no carregamento do componente!");
+
+  },[]);
+
+  useEffect(() =>{
+    console.log("Este useEfect renderiza apenas se o objeto/elemento/componente e ou variável sofrer uma alteração. Devemos colocar o item a ser monitorado no array de dependências: []");
+
+  }, [counter2]);
+  
+  return (
+    <div>
+      <h1>Produtos de INFORMÁTICA - FIPAPI</h1>
+      <p>Os Melhores Produtos do Mercado</p>
+
+      <div>
+        <button onClick={() => setCounter(counter + 1)}>COUNTER - {counter}</button>
+      </div>
+
+      <table className={classes.estilo}>
+        <thead className={classes.tableHeaders}>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>DESCRIÇÃO</th>
+            <th>PREÇO</th>
+            <th>IMAGEM</th>
+            <th>EDITAR</th>
+          </tr>
+        </thead> 
+
+        <tbody>
+          {ListaProdutos.map((produto, indice) => (
+            <tr key={indice} className={classes.tableLine}>
+              <td>{produto.id}</td>
+              <td>{produto.nome}</td>
+              <td>{produto.desc}</td>
+              <td>{produto.preco}</td>
+              <td><img src={produto.img} alt={produto.desc} /></td>
+              <td>
+                <Link to={`/editar/produtos/${produto.id}`}> <Editar/></Link> | <Link to={`/excluir/produtos/${produto.id}`}> <Excluir/> </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+        <tfoot className={classes.tableFooter}>
+          <tr>
+            <td colSpan={6}>PRODUTOS</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
 }
